36149ff359d8f6b7242d1e9e48b8095fef55499c
Paul Bae <pbae@utexas.edu>
2015-03-26 17:00:47 -0500

    redesigned the UI!

--- FreeSpirits/views.py
+++ FreeSpirits/views.py
@@ -9,20 +9,34 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
-
 @app.route('/builder')
 def builder():
     return render_template("builder.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/drinks')
+def drinks_listing():
+    return render_template("drinks.html")
+  
 @app.route('/drinks/<drink>')
 def drinks(drink):
     return render_template(drink + ".html")
 
+@app.route('/ingredients')
+def ingredients_listing():
+    return render_template("ingredients.html")
+  
 @app.route('/ingredients/<ingredient>')
 def ingredients(ingredient):
     return render_template(ingredient + ".html")
 
+@app.route('/users')
+def users_listing():
+    return render_template("users.html")
+
 @app.route('/users/<username>')
 def users(username):
-    print(username)
     return render_template(username + ".html")
--- wsgi.py
+++ wsgi.py
@@ -13,4 +13,5 @@ from FreeSpirits import app
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host="0.0.0.0", port=port)

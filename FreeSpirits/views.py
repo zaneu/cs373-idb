b0e85b9928341b3e187ec5e3d8f06323f038c6c8
Paul Bae <pbae@utexas.edu>
2015-03-26 09:41:37 -0500

    routed drinks and users and ingredients

--- FreeSpirits/views.py
+++ FreeSpirits/views.py
@@ -13,3 +13,15 @@ def index():
 def builder():
     return render_template("builder.html")
 
+@app.route('/drinks/<drink>')
+def drinks(drink):
+    return render_template(drink + ".html")
+
+@app.route('/ingredients/<ingredient>')
+def ingredients(ingredient):
+    return render_template(ingredient + ".html")
+
+@app.route('/users/<username>')
+def users(username):
+    print(username)
+    return render_template(username + ".html")

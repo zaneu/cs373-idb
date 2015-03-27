36e8ba25097b6e8ef9b29e63a575083656eb4c27
larry-liu <liudi1990@utexas.edu>
2015-03-27 02:33:15 +0000

    fixed

--- wsgi.py
+++ wsgi.py
@@ -12,6 +12,6 @@ import os
 from FreeSpirits import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.debug = True
     app.run(host="0.0.0.0", port=port)

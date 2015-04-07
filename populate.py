bb741edf599c1e8739a299b300eb43fb642b4e68
Paul Bae <pbae@utexas.edu>
2015-04-07 16:46:23 -0500

    done with app.db

--- populate.py
+++ populate.py
@@ -2,9 +2,9 @@
 
 import json
 from FreeSpirits import db
-from FreeSpirits import Ingredient
-from FreeSpirits import Drink
-from FreeSpirits import IngredientToDrink
+from FreeSpirits.models import Ingredient
+from FreeSpirits.models import Drink
+from FreeSpirits.models import IngredientToDrink
 
 print("Dropping all tables")
 db.drop_all()
@@ -12,11 +12,12 @@ print("Creating all tables")
 db.create_all()
 
 print("Loading data/ingredients.json")
-ingredients = json.load(with f as open("data/ingredients.json"))
+f = open("data/temp.json")
+ingredients = json.load(f)
+f.close()
 
-for key in ingredients:
-    value = ingredients[key]
-    Ingredient ingredient = Ingredient(
+for value in ingredients:
+    ingredient = Ingredient(
             name=value["name"],
             description=value["description"],
             calories=value["calories"],
@@ -37,35 +38,35 @@ for key in ingredients:
 print("Committing ingredients")
 db.session.commit()
 
-print("Loading data/drinks.json")
-drinks = json.load(with f as open("data/drinks.json"))
+#print("Loading data/drinks.json")
+#drinks = json.load(with f as open("data/drinks.json"))
 
-for key in drinks:
-    value = drinks[key]
+#for key in drinks:
+    #value = drinks[key]
 
-    Drink drink = Drink(
-            name=value["name"],
-            description=value["description"],
-            calories=value["calories"],
-            energy=value["energy"],
-            fats=value["fats"],
-            carbohydrates=value["carbohydrates"],
-            protein=value["protein"],
-            fiber=value["fiber"],
-            sugars=value["sugars"],
-            cholesterol=value["cholesterol"],
-            sodium=value["sodium"],
-            alcohol=value["alcohol"]
-    )
+    #Drink drink = Drink(
+            #name=value["name"],
+            #description=value["description"],
+            #calories=value["calories"],
+            #energy=value["energy"],
+            #fats=value["fats"],
+            #carbohydrates=value["carbohydrates"],
+            #protein=value["protein"],
+            #fiber=value["fiber"],
+            #sugars=value["sugars"],
+            #cholesterol=value["cholesterol"],
+            #sodium=value["sodium"],
+            #alcohol=value["alcohol"]
+    #)
 
-    for ingredient_name in value["ingredients"]:
-        ingredient_quantity = value["ingredients"][ingredient_name]
-        ingredient = Ingredient.query.filter_by(name=ingredient_name).first()
-        link = IngredientToDrink(ingredient_id=ingredient.id, drink_id=drink.id, quantity=ingredient_quantity)
-        db.session.add(link)
+    #for ingredient_name in value["ingredients"]:
+        #ingredient_quantity = value["ingredients"][ingredient_name]
+        #ingredient = Ingredient.query.filter_by(name=ingredient_name).first()
+        #link = IngredientToDrink(ingredient_id=ingredient.id, drink_id=drink.id, quantity=ingredient_quantity)
+        #db.session.add(link)
 
-    db.session.add(drink)
-    print("Added drink: " + value["name"])
+    #db.session.add(drink)
+    #print("Added drink: " + value["name"])
 
-print("Committing drinks")
-db.session.commit()
+#print("Committing drinks")
+#db.session.commit()

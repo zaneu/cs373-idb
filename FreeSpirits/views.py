810cbb6323a37c739b2bb26c94c8b25c28aa1504
Fang Tang <jindtang@utexas.edu>
2015-04-08 16:53:14 -0500

    added page not found handling fix #37

--- FreeSpirits/views.py
+++ FreeSpirits/views.py
@@ -25,6 +25,8 @@ def drinks(drink_id=None):
     if drink_id is None:
         return render_template("drinks.html", drinks=Drink.query.order_by(Drink.name))
     ingredients = []
+    if IngredientToDrink.query.filter_by(drink_id=drink_id).first() is None:
+        return page_not_found(404)
     for ingredient in IngredientToDrink.query.filter_by(drink_id=drink_id):
         ingredients.append(ingredient.quantity + " " + Ingredient.query.filter_by(id=ingredient.ingredient_id).first().name)
     return render_template("drink.html", drink=Drink.query.filter_by(id=drink_id).first(), ingredients=ingredients)
@@ -35,7 +37,10 @@ def drinks(drink_id=None):
 def ingredients(ingredient_id=None):
     if ingredient_id is None:
         return render_template("ingredients.html", ingredients=Ingredient.query.order_by(Ingredient.name))
-    return render_template("ingredient.html", ingredient=Ingredient.query.filter_by(id=ingredient_id).first())
+    ingredient_page = Ingredient.query.filter_by(id=ingredient_id).first()
+    if ingredient_page is None:
+        return page_not_found(404)
+    return render_template("ingredient.html", ingredient=ingredient_page)
 
 @app.route('/users')
 @app.route('/users/')
@@ -43,7 +48,10 @@ def ingredients(ingredient_id=None):
 def users(user_id=None):
     if user_id is None:
         return render_template("users.html", users=User.query.order_by(User.name))
-    return render_template("user.html", user=User.query.filter_by(id=user_id).first())
+    user_page = User.query.filter_by(id=user_id).first()
+    if user_page is None:
+        return page_not_found(404)
+    return render_template("user.html", user=user_page)
 
 @app.errorhandler(404)
 def page_not_found(error):

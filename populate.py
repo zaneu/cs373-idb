9281b0ebcf52d61f872614614e1f8e2c2708e5b4
Paul Bae <pbae@utexas.edu>
2015-04-08 00:21:15 -0500

    removed app db and updated populate
    populate now uses a progress bar

--- populate.py
+++ populate.py
@@ -11,6 +11,7 @@ from FreeSpirits.models import Ingredient
 from FreeSpirits.models import Drink
 from FreeSpirits.models import IngredientToDrink
 from sqlalchemy import func
+from clint.textui import progress
 
 print("Dropping all tables")
 db.drop_all()
@@ -20,11 +21,11 @@ db.create_all()
 print("Loading data/ingredients.json")
 f = open("data/ingredients.json")
 ingredients = json.load(f)
+ingredient_table = {}
 f.close()
 
-for value in ingredients:
+for value in progress.bar(ingredients):
     ingredient = Ingredient(
-            id=value["id"],
             name=value["name"],
             description=value["description"],
             calories=value["calories"],
@@ -40,7 +41,7 @@ for value in ingredients:
     )
 
     db.session.add(ingredient)
-    print("Added ingredient: " + value["name"])
+    ingredient_table[value["id"]] = ingredient
 
 print("Committing ingredients")
 db.session.commit()
@@ -51,8 +52,9 @@ drinks = json.load(f)
 f.close()
 
 count = 1
+drink_count = 1
 
-for value in drinks:
+for value in progress.bar(drinks):
     drink = Drink(
             name=value["name"],
             description=value["description"],
@@ -64,16 +66,11 @@ for value in drinks:
 
     for ingredient_id in value["ingredients"]:
         ingredient_quantity = value["ingredients"][ingredient_id]
-        ingredient = Ingredient.query.filter_by(id=ingredient_id).first()
-        if ingredient is None:
-            print(drink.name)
-            print(ingredient_id)
+        ingredient = ingredient_table[ingredient_id]
+
         link = IngredientToDrink(id=count, ingredient_id=ingredient.id, drink_id=drink.id, quantity=ingredient_quantity)
         db.session.add(link)
         count += 1
-
-    db.session.commit()
-    print("Added drink: " + value["name"])
 
 print("Committing drinks")
 db.session.commit()

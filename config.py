12439d2ed305d0c4a4b07018758587f5ea3357b7
Paul Bae <pbae@utexas.edu>
2015-04-19 11:17:18 -0500

    by default use sqlite, instead of postgres

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # configure sqlalchemy
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/free_spirits'
+# sqlite is used by default for development machines
+# you can setup postgresql in the following link:
+# https://wiki.archlinux.org/index.php/PostgreSQL
+# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/free_spirits'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')

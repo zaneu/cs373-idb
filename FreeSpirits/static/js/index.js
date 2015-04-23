552208ecbcaee4dbb5d4fc2974b4befd41a062ca
Paul Bae <pbae@utexas.edu>
2015-04-23 16:56:42 -0500

    search bar in the layout

--- FreeSpirits/static/js/index.js
+++ FreeSpirits/static/js/index.js
@@ -3,19 +3,17 @@ $(document).ready(function(){
     var path = window.location.pathname;
 
     function redirect_to_search () {
-        var query = $('#navbar-inputdata').val();
+        var query = $('#nputdata').val();
         window.location.href = host + "/search/" + query;
     }
 
-    $('#navbar-inputdata').keydown (function (event) {
+    $('#inputdata').keydown (function (event) {
         if (event.keyCode == 13) {
             redirect_to_search();
-            return false;
         }
-        return true;
     });
 
-    $('#icon-search-for').on('click', function () {
+    $('#searchBtn').on('click', function () {
         redirect_to_search();
     });
 });

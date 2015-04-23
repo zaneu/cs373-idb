c0497c1f3291c27a0eacc1f712cd30f828b28f2c
Paul Bae <pbae@utexas.edu>
2015-04-23 17:55:26 -0500

    index js working now

--- FreeSpirits/static/js/index.js
+++ FreeSpirits/static/js/index.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
     var host = "http://" + window.location.host;
     var path = window.location.pathname;
 
-    function redirect_to_search () {
-        var query = $('#nputdata').val();
+    function redirect_to_index_search () {
+        var query = $('#inputdata').val();
         window.location.href = host + "/search/" + query;
     }
 
     $('#inputdata').keydown (function (event) {
         if (event.keyCode == 13) {
-            redirect_to_search();
+            redirect_to_index_search();
         }
     });
 
     $('#searchBtn').on('click', function () {
-        redirect_to_search();
+        redirect_to_index_search();
     });
 });
